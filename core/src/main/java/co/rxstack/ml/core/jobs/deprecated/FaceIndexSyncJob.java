cb0e7e65efa38ca8c426f5221707d2fe269806aa
mhachem <mahmoudhashim.k@gmail.com>
2018-02-03 20:55:54 +0200

    committed deprecated code

--- core/src/main/java/co/rxstack/ml/core/jobs/deprecated/FaceIndexSyncJob.java
+++ core/src/main/java/co/rxstack/ml/core/jobs/deprecated/FaceIndexSyncJob.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+@Deprecated
 @Component
 public class FaceIndexSyncJob {
 
--- core/src/main/java/co/rxstack/ml/core/jobs/deprecated/FaceModelTrainingJob.java
+++ core/src/main/java/co/rxstack/ml/core/jobs/deprecated/FaceModelTrainingJob.java
@@ -7,6 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+/**
+ * Using Akka Actors
+ */
+@Deprecated
 @Component
 public class FaceModelTrainingJob {
 
